b39e7d105472b25336cfa4908bac2c23f8f22daa
Dana Klassen <klassen.dana@icloud.com>
2026-06-10 16:58:45 -0400

    Remove the block around shell activation in shellHook

--- flake.nix
+++ flake.nix
@@ -19,23 +19,15 @@
             pkgs.k3d
             pkgs.kubectl
             pkgs.kubernetes-helm
+            pkgs.zsh
           ];
 
           shellHook = ''
-
-          if [[ -z "$CLICKHOUSE_DEV_SHELL" ]]; then
             echo "dev shell"
             echo "  k3d     $(k3d version --short 2>/dev/null || echo n/a)"
             echo "  kubectl $(kubectl version --client --short 2>/dev/null || echo n/a)"
             echo "  helm    $(helm version --short 2>/dev/null || echo n/a)"
             echo "  task    $(task --version 2>/dev/null || echo n/a)"
-
-            // Prevent infinite shell spawning when the user is already in a dev shell
-            // wonky 
-            //  [ :TOODO:] fix 
-              export CLICKHOUSE_DEV_SHELL=1
-              exec zsh
-            fi
           '';
         };
       }
